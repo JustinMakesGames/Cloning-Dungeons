ee2610a5fc0c607af7664af95583464f34806179
Ben <benjaminjoeri2007@gmail.com>
2024-03-15 17:35:04 +0100

    Begin Nieuw Inventory Script

---
+++ CloningDungeons/Assets/Ben/Scripts/InventoryScriptableObj.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "InventoryData", menuName = "InvetoryScriptableObj")]
+public class InventoryScriptableObj : ScriptableObject
+{
+    public string itemName;
+    public GameObject itemPrefab;
+    public int inventorySlots;
+
+}
