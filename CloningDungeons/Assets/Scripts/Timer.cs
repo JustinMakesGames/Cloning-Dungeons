b4c3404d60720b3888f856e0717aeeb5ca955eb7
JustinMakesGames <justinlf75@gmail.com>
2024-03-18 15:29:06 +0100

    Some things are done

--- CloningDungeons/Assets/Scripts/Timer.cs
+++ CloningDungeons/Assets/Scripts/Timer.cs
@@ -2,16 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
     public static float timer;
     public TMP_Text timertext;
     public bool cutscene;
+    public GameObject blackscreen;
+    public Image image;
+    private Color black;
+    [SerializeField] private float blackscreenspeed;
+    private bool blackscreenhappened;
 
     private void Start()
     {
-        timer = 300f;
+        timer = 3f;
+        image = blackscreen.GetComponent<Image>();
+        black = image.color;
+
     }
 
     void Update()
@@ -26,14 +35,32 @@ public class Timer : MonoBehaviour
     //FloorToInt zorgt ervoor dat ie getal omlaag afrondt
     void TimerCountdown()
     {
-        timer -= Time.deltaTime;
-        int minutes = Mathf.FloorToInt(timer / 60.0f);
-        int seconds = Mathf.FloorToInt(timer - minutes * 60);
-        timertext.text = minutes.ToString() + ":" + seconds.ToString("00");
+        if (timer > 0)
+        {
+            timer -= Time.deltaTime;
+            int minutes = Mathf.FloorToInt(timer / 60.0f);
+            int seconds = Mathf.FloorToInt(timer - minutes * 60);
+            timertext.text = minutes.ToString() + ":" + seconds.ToString("00");
+        }
+        
 
         if (timer <= 0)
         {
+            if (!blackscreenhappened)
+            {
+                image.color += new Color(0, 0, 0, blackscreenspeed * Time.deltaTime);
+                if (image.color.a >= 1f)
+                {
+                    blackscreenhappened = true;
+                }
+            }
+            
 
+            if (blackscreenhappened)
+            {
+                image.color -= new Color(0, 0, 0, blackscreenspeed * Time.deltaTime);
+            }
+            
         }
     }
 }
