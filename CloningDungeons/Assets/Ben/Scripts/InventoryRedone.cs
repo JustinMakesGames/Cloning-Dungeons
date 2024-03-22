fdbeae424f48810a3421d106dbfb629524935d63
Ben <benjaminjoeri2007@gmail.com>
2024-03-22 14:47:04 +0100

    inventory Redone

--- CloningDungeons/Assets/Ben/Scripts/InventoryRedone.cs
+++ CloningDungeons/Assets/Ben/Scripts/InventoryRedone.cs
@@ -4,7 +4,7 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class UIScript : MonoBehaviour
+public class InventoryRedone : MonoBehaviour
 {   
     public RaycastHit hit;
 
