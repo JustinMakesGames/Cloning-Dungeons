7d848eec368612c8b887bbc1ba9d13eb1d496b90
JustinMakesGames <justinlf75@gmail.com>
2024-04-09 22:55:55 +0200

    Pause button works completely

--- CloningDungeons/Assets/Scripts/Triggerers/PlatformScript.cs
+++ CloningDungeons/Assets/Scripts/Triggerers/PlatformScript.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class PlatformScript : MonoBehaviour
@@ -11,17 +10,14 @@ public class PlatformScript : MonoBehaviour
         "Blue",
         "Green",
         "Yellow"
-
     };
 
     private float time = 2f;
     private float endTimer = 2f;
-
+    private List<int> activatedButtonIndices = new List<int>(); // Store the indices of activated buttons
 
     public Button[] buttons;
     public List<Transform> platforms;
-
-
 
     private void Start()
     {
@@ -36,50 +32,48 @@ public class PlatformScript : MonoBehaviour
     {
         for (int i = 0; i < buttons.Length; i++)
         {
-            if (buttons[i].fullyPressed)
+            if (buttons[i].fullyPressed && !activatedButtonIndices.Contains(i))
             {
+                activatedButtonIndices.Add(i);
                 time = 0;
                 for (int x = 0; x < platforms.Count; x++)
                 {
                     if (platforms[x].CompareTag(tags[i]))
                     {
-                        print("It should fucking work");
                         PlatformChange(platforms[x].gameObject.GetComponent<MeshRenderer>().material,
                             platforms[x].gameObject.GetComponent<Collider>());
                     }
                 }
-                return;
             }
         }
 
         time += Time.deltaTime;
-        if (time < endTimer)
+        if (time >= endTimer)
         {
-            return;
-        }
-
-        for (int i = 0; i < platforms.Count; i++)
-        {
-            TurnOffPlatform(
-                platforms[i].gameObject.GetComponent<MeshRenderer>().material,
-                platforms[i].gameObject.GetComponent<Collider>());
+            TurnOffAllPlatforms();
+            activatedButtonIndices.Clear();
         }
     }
 
-   
-
     private void PlatformChange(Material material, Collider collider)
     {
-   
         material.color = new Color(material.color.r, material.color.g, material.color.b, 1f);
         collider.enabled = true;
-
     }
 
     private void TurnOffPlatform(Material material, Collider collider)
     {
-        
         material.color = new Color(material.color.r, material.color.g, material.color.b, 0.5f);
         collider.enabled = false;
+    }
+
+    private void TurnOffAllPlatforms()
+    {
+        foreach (var platform in platforms)
+        {
+            TurnOffPlatform(
+                platform.gameObject.GetComponent<MeshRenderer>().material,
+                platform.gameObject.GetComponent<Collider>());
+        }
     }
 }
